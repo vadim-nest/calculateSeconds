c6e430166a0a4c08b7184d88588a288bb89a7aaf
Vadim Gnezdyshkin <vadim@gne.me.uk>
2023-07-13 10:46:18 +0100

    Feature: add date picker in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const datePickerProps = {
+    dateFormat: "dd/MM/yyyy - hh:mm",
+    timeInputLabel: 'Time:',
+    showMonthDropdown: true,
+    showYearDropdown: true,
+    showTimeInput: true,
+    dropdownMode: 'select',
+    fixedHeight: true,
+    form: 'external-form'
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(startDate, endDate);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div className='select-box'>
+          <div className='left-select'>
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              {...datePickerProps}
+            />
+          </div>
+          <div className='right-select'>
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+              {...datePickerProps}
+            />
+          </div>
+            <form id='external-form' onSubmit={handleSubmit}>
+              <input type='submit' />
+            </form>
+        </div>
       </header>
     </div>
   );
